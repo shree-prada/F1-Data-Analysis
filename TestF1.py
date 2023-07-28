f424d5cc55ac17e53c3981e55f986538e954626e
MK30698 <patelmayank30698@gmail.com>
2023-07-28 19:41:14 -0400

    commit_race data v1

--- TestF1.py
+++ TestF1.py
@@ -1,6 +1,12 @@
-import fastf1
+import fastf1 as ff1
 import pandas as pd
-
+'''
 session=fastf1.get_event_schedule(2023)
 df=pd.DataFrame(session)
 df.to_csv("2023_f1.csv")
+'''
+
+qualification = ff1.get_session(2022, 'Saudi Arabian Grand Prix')
+qualification.load()
+df=pd.DataFrame(qualification.results)
+df.to_csv("test.csv")
